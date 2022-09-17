acda5280e8cf50712b1626404afa8f6f9389e011
Arthur Eller <arthur.eller@alicredito.com.br>
2022-09-17 15:17:16 -0300

    remove lodash

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 require("dotenv/config");
 const { Telegraf } = require("telegraf");
 const axios = require("axios");
-const _ = require("lodash");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
