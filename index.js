e7f7ad517bea096f5ce911936cdb3e4668c9cf36
Arthur Eller <arthur.eller@alicredito.com.br>
2022-09-17 14:50:32 -0300

    express

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const main = require("./src/main");
+const express = require("express");
 
-setInterval(main.run, 3000);
+const app = express();
+
+app.get("/", (request, response) => {
+  main.run();
+  return response.status(200).json("Server is up");
+});
